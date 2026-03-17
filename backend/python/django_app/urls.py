eb4cc29b62aa4b07376c25a211a99fcfac296b15
Samriddhi Varshney <sam.varshney@outlook.com>
2026-03-18 01:40:41 +0530

    my first changes to repo

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import JsonResponse
 
-def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+def hello_name(request):
+    name = request.GET.get("name", "World")
+    return JsonResponse({"message": f"Hello, {name}!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello/', hello_name),
 ]
